c6d4107718d28e7c97e0fd85a1dc8f21fc15981e
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-08-02 18:18:40 -0400

    refactor: data to login

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## For logging to home
+
+Put some of the email and password in `src/Assets/Data/user.js` in the page login
+That users can log to Home
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/Assets/Data/user.js
+++ src/Assets/Data/user.js
@@ -1,8 +1,37 @@
+// Users to Login
 const User = [
   {
-    email: 'dorime@gmail.com',
-    password: 'dorime'
-  }
-]
+    email: "dorime@gmail.com",
+    password: "dorime",
+  },
+  {
+    email: "angelonivea@gmail.com",
+    password: "dt7b3voE8MFOC8N9N",
+  },
+  {
+    email: "alenelamonivar@gmail.com",
+    password: "dt7b3voE8MFOC8N9N",
+  },
+  {
+    email: "nicolas@gmail.com",
+    password: "dt7b3voE8MFOC8N9N",
+  },
+  {
+    email: "brendom@gmail.com",
+    password: "dt7b3voE8MFOC8N9N",
+  },
+  {
+    email: "chrissy@gmail.com",
+    password: "dt7b3voE8MFOC8N9N",
+  },
+  {
+    email: "cynthia@gmail.com",
+    password: "dt7b3voE8MFOC8N9N",
+  },
+  {
+    email: "gerald@gmail.com",
+    password: "dt7b3voE8MFOC8N9N",
+  },
+];
 
 export { User };
