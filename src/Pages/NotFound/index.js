a6b67255d1b275d8eaa74433f0c9814459c3a257
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-08-02 18:07:29 -0400

    feat: error not found page

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { HeaderPage } from "../../Components/HeaderPage";
+import { FooterPage } from "../../Components/FooterPage";
+import loader from "../../Assets/Img/rolling-cat-cat-rolling.webp";
+import "./NotFound.css";
 
 const NotFound = () => {
-  return <p>Not found</p>;
+  return (
+    <Fragment>
+      <HeaderPage />
+      <div className="container-error">
+        <div className="container">
+          <img src={loader} alt="rolling cat" loading="lazy" width="80%" />
+          <p className="error-title">Error Not Found</p>
+          <p className="error-message">Enjoy this cat</p>
+        </div>
+      </div>
+      <FooterPage />
+    </Fragment>
+  );
 };
 
 export { NotFound };
